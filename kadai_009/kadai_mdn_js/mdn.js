8b7cb63d37a96f8b92b45b0d21a381925b0bfbc7
shiori74 <143091326+shiori74@users.noreply.github.com>
2023-10-22 18:57:02 +0900

    Add files via upload

---
+++ kadai_009/kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+let dateName = new Date('2023, 9, 22');
+
+const moonLanding = new Date('9 20, 69 00:20:18');
+
+const birthday = new Date('August 22, 1975 23:15:30');
+const data1 = birthday.getDate();
+
+
+let year = dateName.getFullYear();
+let month = dateName.getMonth();
+let day = dateName.getDay();
+
+console.log(year + '年' + month + '月' + data1 + '日');
